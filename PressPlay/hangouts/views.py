6ad3fe4a526c795a671abe9139e07d50a425bf1a
mike-n-7 <mnoseworthy7@gmail.com>
2014-02-22 18:02:28 -0500

    Works with generic number of users.

--- PressPlay/hangouts/views.py
+++ PressPlay/hangouts/views.py
@@ -9,36 +9,55 @@ def index(request):
 	context = RequestContext(request, {},)
 	return HttpResponse(template.render(context))
 
+# Given a request, returns a list containing all the entered soundhound user ids.
+def get_users(client, request):
+	ids = []
+	# Each user input has the name with the format user_name#.
+	for key,val in request.POST.iteritems():
+		if "user_name" in key:
+			user_id = client.get('/resolve', url='https://soundcloud.com/' + val).id
+			ids.append(user_id)
+	return ids
+
+def get_sorted_tracks(client, ids):
+	tracks = []
+	track_ids = {}
+	# Find all tracks that users have favourited.
+	for user in ids:
+		tracks = tracks + list(client.get('/users/' + str(user) + '/favorites'))
+	print "Users: ",
+	print ids
+	print "Tracks: ",
+	print tracks
+	# Count how many users have favourited each song.
+	for t in tracks:
+		print t.title, t.id
+		if t.id in track_ids:
+			track_ids[t.id] += 1
+		else:
+			track_ids[t.id] = 1
+
+	print track_ids
+
+	# Return a list of sorted track ids based on each songs rank.
+	sorted_tracks = sorted(track_ids, key=track_ids.get)
+	sorted_tracks.reverse()
+	print sorted_tracks
+	return sorted_tracks
+
 # Playlist will contain a customized soundcloud playlist.
 def playlist(request):
 	client = soundcloud.Client( 
 		client_id='d05accec0d8806ca775fd78523f6832a',
 		client_secret='e78f9f003214b112a1160561089a9182',
 		username='mike_n_7',
-		password='pressplay'	
+		password='pressplay'
 	)
 
-	user_id1 = client.get('/resolve', url='https://soundcloud.com/' + request.POST['user1_name']).id
-	user_id2 = client.get('/resolve', url='https://soundcloud.com/' + request.POST['user2_name']).id
-	fav1 = client.get('/users/' + str(user_id1) + '/favorites')
-	fav2 = client.get('/users/' + str(user_id2) + '/favorites')
+	users = get_users(client, request)
+	ranked_tracks = get_sorted_tracks(client, users)
 
-	track_ids1 = []
-	track_ids2 = []
-	for f in fav1:
-		track_ids1.append(f.id)
-	for f in fav2:
-		track_ids2.append(f.id)
-
-	print track_ids1
-	print track_ids2
-	track_ids = []
-	for t in track_ids1:
-		if t in track_ids2:
-			track_ids.append(t)
-	print track_ids
-
-	tracks = map(lambda id: dict(id=id), track_ids)
+	tracks = map(lambda id: dict(id=id), ranked_tracks)
 	playlist = client.post('/playlists', playlist={
 		'title': 'New playlist',
 		'sharing': 'public',
