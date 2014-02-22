721f078a6a85df2d9c59cff673b01591c7306cdc
mike-n-7 <mnoseworthy7@gmail.com>
2014-02-22 13:47:55 -0500

    Default views added.

--- PressPlay/hangouts/views.py
+++ PressPlay/hangouts/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.http import HttpResponse
+
+# Index will contain a form for entering user information to generate the playlist.
+def index(request):
+	return HttpResponse("INDEX")
+
+# Playlist will contain a customized soundcloud playlist.
+def playlist(request):
+	return HttpResponse("PLAYLIST")
