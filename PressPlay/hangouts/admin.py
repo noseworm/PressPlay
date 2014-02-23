a1e7557e1a984f4bfa37c4c9753f200e45670094
mike-n-7 <mnoseworthy7@gmail.com>
2014-02-23 04:46:32 -0500

    Database fully implemented to save time.

---
+++ PressPlay/hangouts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from hangouts.models import Users, Playlists, Tracks
+
+admin.site.register(Users)
+admin.site.register(Playlists)
+admin.site.register(Tracks)
--- PressPlay/hangouts/views.py
+++ PressPlay/hangouts/views.py
@@ -20,49 +20,100 @@ def get_users(client, request):
 			ids.append(user_id)
 	return ids
 
-def populate_user(user_name):
-	user_id = client.get('/resolve', url='https://soundcloud.com/' + val).id
-	user = User(user_id=user_id, user_name=user_name)
+def populate_user(client, user_name):
+	user_id = str(client.get('/resolve', url='https://soundcloud.com/' + user_name).id)
+	user = Users(user_id=user_id, user_name=user_name)
 	user.save()
 
-	favourites = client.get('/users/' + user_id + '/favorites')
+	favourites = list(client.get('/users/' + user_id + '/favorites'))
+	print "GOT FAVOURITES: " + str(len(favourites))
 	for fav in favourites:
 		# Create track if it doesn't already exist in the database.
-		try:
-			track = Tracks.objects.filter(track_id=fav.id)
-		except:
-			track = Track(track_id=fav.id, track_name=fav.title, genre=fav.genre)
-			track.save()
+
+		print "SEARCHING FOR TRACK..."
+		tracks3 = Tracks.objects.filter(track_id=str(fav.id))
+		if len(tracks3) == 0:
+			print "ADDING TRACK..."
+			try:
+				title=fav.title
+			except:
+				title=""
+			try:
+				genre=fav.genre
+			except:
+				genre=""
+			t3 = Tracks(track_id=str(fav.id), track_title=title, genre=genre)
+			t3.save()
+		else:
+			t3 = tracks3[0]
 		# Create a favourite relationship.
-		fav_rel = Favourites(uesr=user, track=track)
+		fav_rel = Favourites(user=user, track=t3)
 		fav_rel.save()
 
-	playlists = client.get('/users/' + str(user) + '/playlists')
+	playlists = list(client.get('/users/' + str(user.user_id) + '/playlists'))
 	for plist in playlists:
-		# Create playlist if it doesn't already exist in the database.
-		try:
-			playlist = Playlists()
-		except:
-
+		# Create playlist.
+		playlist = Playlists(playlist_id=str(plist.id))
+		playlist.save()
 		# Create relationship between user and playlist.
-		for track in plist.tracks:
+		for t in plist.tracks:
 			# Create track if it doesn't already exist in the database.
-
+			tracks2 = Tracks.objects.filter(track_id=str(t[u'id']))
+			if len(tracks2) == 0:
+				print "ADDING TRACK..."
+				try:
+					title=t.title
+				except:
+					title=""
+				try:
+					genre=t.genre
+				except:
+					genre=""
+				t2 = Tracks(track_id=str(t[u'id']), track_title=title, genre=genre)
+				t2.save()
+			else:
+				t2 = tracks2[0]
 			# Create relationship between playlist and track.
+			playlist_track = TrackOnPlaylist(track=t2, playlist=playlist)
+			playlist_track.save()
 
-	following = client.get('/users/' + str(user) + '/followings')
+	following = list(client.get('/users/' + str(user.user_id) + '/followings'))
 	for fol in following:
 		# Create artist if it doesn't already exist.
-
+		artists = Users.objects.filter(user_id=str(fol.id))
+		if len(artists) == 0:
+			print "ADDING ARTIST..."
+			artist = Users(user_id=str(fol.id), user_name=fol.username)
+			artist.save()
+		else:
+			artist = artists[0]
 		# Create relationship between artist and user.
+		following = Following(user=user, artist=artist)
+		following.save()
 
-		tracks = client.get('/users/' + str(fol.id) + '/tracks')
-		for t in tracks:
+		tracks1 = list(client.get('/users/' + str(fol.id) + '/tracks'))
+		for t in tracks1:
 			# Create track if it doesn't already exist.
-
+			ts = Tracks.objects.filter(track_id=str(t.id))
+			if len(ts) == 0:
+				print "ADDING TRACK..."
+				try:
+					title=t.title
+				except:
+					title=""
+				try:
+					genre=t.genre
+				except:
+					genre=""
+				t1 = Tracks(track_id=str(t.id), track_title=title, genre=genre)
+				t1.save()
+			else:
+				t1 = ts[0]
 			# Create relationship between artist and track.
+			artist_of = ArtistOf(user=artist, track=t1)
+			artist_of.save()
 
-def get_sorted_tracks(client, ids):
+def get_sorted_tracks():
 	fav_tracks = []
 	playlists = []
 	favs_pts = 10
@@ -73,49 +124,37 @@ def get_sorted_tracks(client, ids):
 	track_ids = {}
 
 	# Find all tracks that users have favourited.
-	for user in ids:
-		fav_tracks = fav_tracks + list(client.get('/users/' + str(user) + '/favorites'))
-			
+	favourites = Favourites.objects.order_by('track__track_title')
+	for t1 in favourites:
+		fav_tracks.append(t1.track)
+	
 	# Count how many users have favourited each song.
 	for t in fav_tracks:
-		if t.id in track_ids:
-			track_ids[t.id] += favs_pts
+		print t.track_id, t.track_title
+		if t.track_id in track_ids:
+			track_ids[t.track_id] += favs_pts
 		else:
-			track_ids[t.id] = favs_pts
+			track_ids[t.track_id] = favs_pts
 
 	#Find all the playlists for each user
-	for user in ids:
-		playlists = playlists + list(client.get('/users/' + str(user) + '/playlists'))
-
 	#Count how many user have a song in one of their playlists
-	for p in playlists:
-		for t in p.tracks:
-			if t[u'id'] in track_ids:
-				track_ids[t[u'id']] += playlists_pts
-			else:
-				track_ids[t[u'id']] = playlists_pts
+	tracks_on_playlists = TrackOnPlaylist.objects.order_by('track__track_title')
+	for p in tracks_on_playlists:
+		if p.track.track_id in track_ids:
+			track_ids[p.track.track_id] += playlists_pts
+		else:
+			track_ids[p.track.track_id] = playlists_pts
 
 
 	#If we have enough songs, we don't need to get followings
 	if len(track_ids) < max_tracks: 
-		print "GET FOLLOWERS"
-		# Get the list of users followed by each user. 
-		following = []
-		for user in ids:
-			print "GET FOLLOWERS: USER " + str(user)
-			following = following + list(client.get('/users/' + str(user) + '/followings'))
-
-		print "GET TRACKS"
-		# Get a list of tracks that each follower has created.
-		tracks_by_following = []
-		for f in following:
-			tracks_by_following = tracks_by_following + list(client.get('/users/' + str(f.id) + '/tracks'))
+		tracks_by_following = ArtistOf.objects.order_by('track__track_title')
 		# Increase the rank for each track in the followers.
 		for t in tracks_by_following:
-			if t.id in track_ids:
-				track_ids[t.id] += following_points
+			if t.track.track_id in track_ids:
+				track_ids[t.track.track_id] += following_points
 			else:
-				track_ids[t.id] = following_points
+				track_ids[t.track.track_id] = following_points
 
 	#Establishes a threshold (average)
 	sum_pts = 0
@@ -131,16 +170,15 @@ def get_sorted_tracks(client, ids):
 
 	# Return a list of sorted track ids based on each songs rank.
 
+	print track_ids
 	sorted_tracks = sorted(track_ids, key=track_ids.get)
 	sorted_tracks.reverse()
-
-	"""for key, val in sorted_tracks.items():
-		curr_val = sorted_tracks[key]"""
 
 	return sorted_tracks
 
 # Playlist will contain a customized soundcloud playlist.
 def playlist(request):
+
 	client = soundcloud.Client( 
 		client_id='d05accec0d8806ca775fd78523f6832a',
 		client_secret='e78f9f003214b112a1160561089a9182',
@@ -148,8 +186,12 @@ def playlist(request):
 		password='pressplay'
 	)
 
-	users = get_users(client, request)
-	ranked_tracks = get_sorted_tracks(client, users)
+	for key,val in request.POST.iteritems():
+		if "user_name" in key:
+			if len(Users.objects.filter(user_name=str(val))) == 0:
+				populate_user(client, val)
+
+	ranked_tracks = get_sorted_tracks()
 
 	tracks = map(lambda id: dict(id=id), ranked_tracks)
 	playlist = client.post('/playlists', playlist={
